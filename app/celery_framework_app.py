25d6b52dfb5f8289e99053b8462d20b8a7f1baf2
Mahesh <mahesh@Maheshs-MacBook-Air.local>
2025-11-30 22:52:15 +0530

    final working tmp

---
+++ app/celery_framework_app.py
@@ -0,0 +1,14 @@
+from omegaconf import OmegaConf
+from celery_framework import create_app
+from price_app.config.paths import CELERY_CONFIG
+
+# Load YAML config
+cfg = OmegaConf.load(CELERY_CONFIG)
+
+celery_framework_app = create_app(cfg)
+
+# Expose the celery instance
+app = celery_framework_app.app
+
+# For debugging
+print("Loaded tasks:", celery_framework_app.list_tasks())
