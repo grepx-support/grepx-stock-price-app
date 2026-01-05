9492ec8310f5e9d8689b340bb8fcd0f1910054ac
Nayan Thakre <nayan.thakre@grepx.sg>
2026-01-06 01:37:54 +0530

    update the init file to get get_app

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,5 +1,5 @@
 """Main package."""
 
-from .main import app, get_connection, get_database, get_collection, config, connections
+from .main import app, get_connection, get_database, get_collection, get_app, config, connections
 
-__all__ = ["app", "get_collection", "get_connection", "get_database", "config", "connections"]
+__all__ = ["app", "get_collection", "get_connection", "get_database", "get_app", "config", "connections"]
